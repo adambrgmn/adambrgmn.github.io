552bc8ea0413c7fb9e777c3ca0ed3fc50a0d1f39
Adam Bergman <adam@fransvilhelm.com>
2016-08-30 15:33:04 +0200

    Streamlining buildprocess

--- bin/publish.js
+++ bin/publish.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 
-const inquirer = require('inquirer');
+// const inquirer = require('inquirer');
 const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const exec = require('child-process-promise').exec;
 const path = require('path');
 const cheerio = require('cheerio');
 
+const rootDir = path.join(__dirname, '..');
+
 const readIndex = () => {
-  const indexPath = path.join(__dirname, '..', 'build', 'index.html');
+  const indexPath = path.join(rootDir, 'build', 'index.html');
   return fs.readFileAsync(indexPath);
 };
 
@@ -34,26 +36,33 @@ const transformPaths = content => {
 };
 
 const writeToIndex = content => {
-  const file = path.join(__dirname, '..', 'index.html');
+  const file = path.join(rootDir, 'index.html');
   return fs.writeFileAsync(file, content, 'utf-8');
 };
 
-const questions = [
-  {
-    type: 'confirm',
-    name: 'onMaster',
-    message: 'Are you on branch master?',
-    default: false,
-  },
-];
+const checkIfOnMaster = () => new Promise((resolve, reject) => {
+  exec('git rev-parse --abbrev-ref HEAD', { cwd: rootDir })
+    .then(({ stdout }) => {
+      const branch = stdout.trim();
+      if (branch === 'master') return resolve();
+      const e = new Error(
+        'Even though everything is setup you have to be on branch master to push to GitHub'
+      );
+      return reject(e);
+    });
+});
 
-inquirer
-  .prompt(questions)
-  .then(answers => console.log(answers));
+const addChanges = () => exec('git add --all', { cwd: rootDir });
+const commitChanges = () => exec('git commit -m "Project built"', { cwd: rootDir });
+const pushToOrigin = () => exec('git push -u origin master', { cwd: rootDir });
 
-// exec('webpack')
-//   .then(readIndex)
-//   .then(transformPaths)
-//   .then(writeToIndex)
-//   .then(() => console.log('All done!'))
-//   .catch(err => console.error('Error:', err));
+exec('npm run build', { cwd: rootDir })
+  .then(readIndex)
+  .then(transformPaths)
+  .then(writeToIndex)
+  .then(checkIfOnMaster)
+  .then(addChanges)
+  .then(commitChanges)
+  .then(pushToOrigin)
+  .then(() => console.log('All done!'))
+  .catch(err => console.error(`${err.name}: ${err.message}`));
